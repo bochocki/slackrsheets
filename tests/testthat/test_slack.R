32312b3d59b232b4f5b34762a9c2cd225b6e535d
Brad Ochocki <brad.ochocki@gmail.com>
2017-05-07 19:44:00 -0500

    setting up  slack tests

---
+++ tests/testthat/test_slack.R
@@ -0,0 +1,12 @@
+text <- "  I FAIL -D for   Brad +1 -ow "
+text <- "  I FAIL -D for   Brad -1 -ow "
+text <- "  I FAIL -D -1 -ow "
+text <- "  I FAIL -D +1 -ow "
+text <- "  I FAIL +2-D  +1 -ow "
+text <- "  I FAIL+2 -D  +1 -ow "
+text <- "  I FAIL+2 -D -ow "
+text <- "  42 for   Brad +1 -ow "
+text <- "  42  +1 -ow "
+text <- "  42  -ow "
+
+slack_parse(text,"X13")
