848cc8c1ef8fc3eb38ad273ffdfb0e5752ee6f73
Brad Ochocki <brad.ochocki@gmail.com>
2017-05-08 14:33:14 -0500

    edit return

--- R/api.R
+++ R/api.R
@@ -161,12 +161,13 @@ mini <- function(text, user_name, response_url, channel_name) {
 
     # cat(paste0(user_name, " called LINK at ", Sys.time(),"\n"))
     # cat(paste0(user_name,"'s input was: ", text))
-    return(
-      slack_message(response_url,
-                    channel = channel_name,
-                    user_name = user_name,
-                    text = get_link("key.txt"),
-                    private = TRUE))
+    # slack_message(response_url,
+    #               channel = channel_name,
+    #               user_name = user_name,
+    #               text = get_link("key.txt"),
+    #               private = TRUE)
+
+    return(get_link("key.txt"))
 
   } else {
 
