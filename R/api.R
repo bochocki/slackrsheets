eac52f6d63d6b3836ca7fc58214ff860b639f399
Brad Ochocki <brad.ochocki@gmail.com>
2017-05-07 21:52:17 -0500

    add missing comma

--- R/api.R
+++ R/api.R
@@ -95,7 +95,7 @@ rank <- function(text, user_name, response_url){
   slack_message(response_url,
                 channel = channel_name,
                 user_name = user_name,
-                text = paste0("```",matrix(slackrsheets::print_board("A", r)),"```", collapse="\n")
+                text = paste0("```",matrix(slackrsheets::print_board("A", r)),"```", collapse="\n"),
                 private = TRUE)
 
   #paste0(paste0(matrix(slackrsheets::print_board(text, r)),collapse="\n")),
